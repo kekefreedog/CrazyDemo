bc3f733d7e0dd8e5a6411d6366da09b8fc4f2db3
kekefreedog <70959083+kekefreedog@users.noreply.github.com>
2024-10-20 23:25:20 +0200

    Push builded script

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * Page Index
+ *
+ * Index that redirect to the index of your app
+ * 
+ * !!! Don't touch this script please !!!
+ *
+ * PHP version 8.1.2
+ *
+ * @package    kzarshenas/crazyphp
+ * @author     kekefreedog <kevin.zarshenas@gmail.com>
+ * @copyright  2022-2024 Kévin Zarshenas
+ */
+
+# Dev mode
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+/** 
+ * Register Index
+ */
+require_once __DIR__.'/../app/Index.php';
